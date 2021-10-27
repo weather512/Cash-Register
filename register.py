80d8d1498fed1986a46d854459ba7363b451ef14
weather512 <47484533+weather512@users.noreply.github.com>
2021-10-27 11:30:54 -0400

    Create register.py

---
+++ register.py
@@ -0,0 +1,32 @@
+#Void/Refund: -
+#Insert price: +
+#Subtotal: =
+#Complete sale: Enter
+
+import keyboard
+
+print("Modes:\nAdd: 1\nSubtract: 2\nSubtotal: 3\nComplete Sale (Only after getting the subtotal): 4")
+subtotal=0.00
+flag=False
+canComplete=False
+while not flag:
+	mode=int(input("Mode: "))
+	if mode == 1:
+		price=input("$")
+		subtotal+=float(price)
+		canComplete=False
+	elif mode==2:
+		price=input("$")
+		subtotal-=float(price)
+		canComplete=False
+	elif mode==3:
+		form="${:,.2f}".format(subtotal)
+		print("Subtotal: {}".format(form))
+		canComplete=True
+	elif mode==4 and canComplete:
+		moneyGiven=input("$")
+		change=float(moneyGiven)-subtotal
+		flag=True
+		changeForm="${:,.2f}".format(change)
+print("Change: {}".format(changeForm))
+input("Press any key to exit")
