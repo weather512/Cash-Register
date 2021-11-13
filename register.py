c2aba9b4747da876523a4f888e6a1b3aeaaf1179
weather512 <47484533+weather512@users.noreply.github.com>
2021-11-12 20:03:02 -0500

    Update register.py

--- register.py
+++ register.py
@@ -7,15 +7,14 @@ under=False
 index=0
 departments=[]
 numDepartments=0
+canComplete=False
 
-#For future development regarding departments
 while not under:
 	numDepartments=int(input("Number of departments (maximum is 26): "))
 	if numDepartments>0 and numDepartments<=26:
 		under=True
 
 letters=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
-#lettersX=[None]*(numDepartments-1)
 lettersX=[]
 
 def add():
@@ -28,12 +27,9 @@ def add():
 	subtotal+=(price/100)
 	num=""
 	print("\n")
-	#print("Hi")
 
 while index<numDepartments:
-	#lettersX[num]=letters[num]
 	lettersX.append(letters[index])
-	#print(letters[y])
 	index+=1
 
 for x in lettersX:
@@ -57,28 +53,37 @@ def subtract():
 	subtotal-=(price/100)
 	num=""
 	print("\n")
+	canComplete=False
 
 def getSubtotal():
-	print("Subtotal: {}".format(subtotal))
+	global canComplete
+	form="${:,.2f}".format(subtotal)
+	print("Subtotal: {}".format(form))
+	print("Before: {}".format(canComplete))
+	canComplete=True
+	print("After: {}".format(canComplete))
 
-# def Main():
-
-# 	for i in range(10):
-# 		keyboard.add_hotkey(str(i),partial(type,i))
-
-# 	keyboard.add_hotkey('+',add)
-# 	keyboard.add_hotkey('-',subtract)
-# 	keyboard.add_hotkey('=',getSubtotal)
-
-# 	keyboard.wait('esc')
-
-# Main()
+def complete():
+	global subtotal
+	global num
+	if canComplete:
+		if num=="":
+			moneyGiven=0
+		else:
+			moneyGiven=int(num)
+		moneyGiven=float(moneyGiven/100)			
+		change=moneyGiven-subtotal
+		changeForm="${:,.2f}".format(change)
+		print("Change {}".format(changeForm))
+	else:
+		print("You must obtain the subtotal before completing a sale. Input money again.\n")
+		num=""
 
 for i in range(10):
 	keyboard.add_hotkey(str(i),partial(type,i))
 
-keyboard.add_hotkey('+',add)
 keyboard.add_hotkey('-',subtract)
 keyboard.add_hotkey('=',getSubtotal)
+keyboard.add_hotkey('enter',complete)
 
 keyboard.wait('esc')
